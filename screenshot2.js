223bee3ea767ca2a88434d1f41daf2514a7a1bd6
Tray Fleary <tflearydev@gmail.com>
2020-01-01 19:27:07 -0500

    testing different ways to iterate through classes headless browser

--- screenshot2.js
+++ screenshot2.js
@@ -16,15 +16,52 @@ puppeteer.launch({ headless: true }).then(async browser => {
   const page = await browser.newPage()
   await page.setViewport({ width: 800, height: 600 })
  
-  console.log(`Testing adblocker plugin..`)
-  await page.goto('https://www.vanityfair.com')
-  await page.waitFor(1000)
-  await page.screenshot({ path: 'adblocker.png', fullPage: true })
+//   console.log(`Testing adblocker plugin..`)
+//   await page.goto('https://www.vanityfair.com')
+//   await page.waitFor(1000)
+//   await page.screenshot({ path: 'adblocker.png', fullPage: true })
  
   console.log(`Testing the stealth plugin..`)
-  await page.goto('https://bot.sannysoft.com')
-  await page.waitFor(5000)
-  await page.screenshot({ path: 'stealth.png', fullPage: true })
+  await page.goto('https://footlocker.com/product/Nike-Air-Foamposite-Pro---Men-s/V0369001.html')
+  await page.waitFor(5000);
+
+//   await page.evaluate(() => {
+//     Array.from(document.querySelectorAll( '.c-form-field')).filter(element => 
+//       element.textContent == '08.0' )[0].click();
+//  });
+
+// await page.evaluate(() => {
+//     let elements = document.getElementsByClassName('.c-form-field')[0];
+//     for (let element of elements)
+//         element.click();
+// });
+
+
+
+const query = "07.5";
+
+page.evaluate(query => {
+  const elements = [...document.querySelectorAll('.c-form-field')];
+
+  // Either use .find or .filter, comment one of these
+  // find element with find
+  const targetElement = elements.find(e => e.innerText.includes(query));
+
+  // OR, find element with filter
+  // const targetElement = elements.filter(e => e.innerText.includes(query))[0];
+
+  // make sure the element exists, and only then click it
+  targetElement && targetElement.click();
+}, query)
+
+
+
+
+
+
+//   let selector = 'c-form-field'[2];
+//   await page.evaluate((selector) => document.querySelector(selector).click(), selector); 
+  await page.screenshot({ path: 'stealth2.png', fullPage: true })
  
   console.log(`All done, check the screenshots. ✨`)
   await browser.close()
